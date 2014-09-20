d4f26fbcee17d1cdf3eb2600dcb39cd458613687
Nicholas Drane <nicholasdrane@gmail.com>
2014-09-20 19:57:10 -0400

    Homepage finished with mailto incorporated

--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import requests
 
 from django.shortcuts import render
@@ -20,8 +21,8 @@ def index(request):
     <!-- Bootstrap core CSS -->
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom styles for this template -->
-    <link href="cover.css" rel="stylesheet">
+    <!-- Homepage CSS -->
+    <link href="https://s3.amazonaws.com/sparestub/static/homepage.css" rel="stylesheet">
   </head>
 
   <body>
@@ -38,28 +39,13 @@ def index(request):
           </div>
 
           <div class="inner cover">
-            <h1 class="cover-heading">Cover your page.</h1>
-            <p class="lead">
-                Imagine this:
-            </p>
-            <p class="lead">
-                Joey purchased two tickets to an event, and his friend bails. With such short notice, he can’t find anyone else to go. Poor Joey!
-            </p>
-            <p class="lead">
-                Across town, Susie wants to go to the event, but has no ticket and no one to go with. Poor Susie!
-            </p>
-            <p class="lead">
-                Introducing, SpareStub! We will help connect Joey and Susie. After reviewing each other’s profile and messaging a bit, Susie will not only buy Joey’s extra ticket, but also accompany him to the event!
-            </p>
-            <p class="lead">
-                Convenient. Social. SpareStub. Coming: Spring 2015
-            </p>
-            <p class="lead">
-                As we develop SpareStub, we would love ideas, feedback and Beta users! If you would like periodic updates on SpareStub development and release, or would like to be included in helping to design and test the product, click below and send us an email! 
-            </p>
-            <p class="lead">
-              <a href="#" class="btn btn-lg btn-default">Beta Signup</a>
-            </p>
+            <h1 class="cover-heading" style="text-align: center">Imagine this:</h1>
+            <p class="lead" style="text-align: center">Joey purchased two tickets to an event, but his friend bails and he has no one to go with.<br> Poor Joey!</p>
+            <p class="lead" style="text-align: center">Across town, Susie wants to go to the event, but has no ticket and no one to go with. Poor Susie!</p>
+            <p class="lead" style="text-align: center"><b>Introducing SpareStub!</b> We will help connect Joey and Susie. After reviewing each other’s profiles and messaging a bit, Susie will not only buy Joey's extra ticket but also accompany him to the event.</p>
+            <h2 class="cover-heading" style="text-align: center"><i>Convenient. Social. SpareStub.</i><br> Coming: Spring 2015</h2>
+            <p class="lead" style="font-size: 100%" style="text-align: center">As we develop SpareStub, we would love ideas, feedback and Beta users! If you would like periodic updates on SpareStub development and release, or would like to be included in helping to design and test the product, click below and send us an email! </p>
+            <p class="lead" style="text-align: center"><a href="mailto:stephanie@sparestub.com" class="btn btn-lg btn-default">Sign Up Form</a></p>
           </div>
 
         </div>
