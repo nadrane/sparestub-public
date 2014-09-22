aa93dbe7e98fdb5c8efe597649e4beecaf5672ae
Nicholas Drane <nicholasdrane@gmail.com>
2014-09-21 22:17:06 -0400

    Changed the app name 'hello' to contact.

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -39,13 +39,13 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello'
+    'contact',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -3,15 +3,15 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
+import contact.views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'gettingstarted.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^contact/$', hello.views.contact, name='contact'),
+    url(r'^$', contact.views.index, name='index'),
+    url(r'^contact/$', contact.views.contact, name='contact'),
     url(r'^admin/', include(admin.site.urls)),
 
 )
