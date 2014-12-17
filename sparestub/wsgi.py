0ba4cc3d5f1fc59d65f25101f9f1556ad955fbf3
Nicholas Drane <nicholasdrane@gmail.com>
2014-12-17 10:28:17 -0500

    Set up new relic

--- sparestub/wsgi.py
+++ sparestub/wsgi.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import newrelic.agent
+newrelic.agent.initialize('newrelic.ini')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sparestub.settings.production")
 
 from django.core.wsgi import get_wsgi_application
