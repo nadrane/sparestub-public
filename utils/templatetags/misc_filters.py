a8cbb200ff0cde8286f13e9096c0f5e965788f97
kennydust <klai@kiboo.com>
2015-03-20 10:44:52 -0500

    fixing bug in currency

--- utils/templatetags/misc_filters.py
+++ utils/templatetags/misc_filters.py
@@ -1,6 +1,15 @@
 from django.template import Library
 from django.template.defaultfilters import stringfilter
 from django.contrib.humanize.templatetags.humanize import intcomma
+from logentries import LogentriesHandler
+import logging
+import time
+
+# temp
+log = logging.getLogger('logentries')
+log.setLevel(logging.INFO)
+handler = LogentriesHandler('28379e13-d9b8-434f-a233-7ec9369d2fcb')
+log.addHandler(handler)
 
 register = Library()
 
@@ -28,5 +37,8 @@ def format_stripe_price(value):
 
 @register.filter(is_safe=True)
 def currency(dollars):
+    if not dollars :
+       log.warn("dollars was null or string empty")
+       return dollars
     dollars = round(float(dollars), 2)
     return "$%s%s" % (intcomma(int(dollars)), ("%0.2f" % dollars)[-3:])
